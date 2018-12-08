e5a2cbabcfa835cb86805a1be7972245ce20279d
Carlo Francisco <carlo@jcfrancisco.com>
2018-12-08 15:57:01 -0500

    More comments

--- day8.hs
+++ day8.hs
@@ -1,11 +1,17 @@
 import           Data.List.Split
 
+-------------------------------------
+-- Parsing the string into a Tree  --
+-------------------------------------
 type Child = Tree
 type Metadata = Int
 data Tree = Leaf [Metadata] | Tree [Child] [Metadata]
   deriving (Show)
 
--- First in tuple is sum (part 1), second is "value" (part 2)
+-- Takes in the input string of the problem and returns
+-- a tuple. First item in the tuple is the answer to the
+-- first part of the problem, second item is the answer
+-- to the second part of the problem ("value").
 process :: String -> (Int, Int)
 process s = (sumMetadata tree, findValue tree)
   where tree = (convertToTree . toInts) s
@@ -16,6 +22,13 @@ toInts = (map read) . (splitOn " ")
 convertToTree :: [Int] -> Tree
 convertToTree = fst . convertToTreeAndRest
 
+-- Given a list of integers, parse the next tree
+-- in that list of integers. Return a tuple,
+-- where the first value is the tree and the second
+-- is the rest of the unparsed list.
+--
+-- TODO: Learn the more Haskell-y idioms which to write parsers
+-- like this, lol.
 convertToTreeAndRest :: [Int] -> (Tree, [Int])
 convertToTreeAndRest ints =
   case childCount of
@@ -43,6 +56,10 @@ appendNextChild (childrenSoFar, ints) =
   (childrenSoFar ++ [fst nextTreeAndRest], snd nextTreeAndRest)
     where nextTreeAndRest = convertToTreeAndRest ints
 
+-------------------------------------
+-- Actual Sum and Value algorithms --
+-------------------------------------
+
 sumMetadata :: Tree -> Int
 sumMetadata (Leaf metadata) = sum metadata
 sumMetadata (Tree children metadata) = sum metadata + sum (map sumMetadata children)
@@ -60,5 +77,3 @@ main = do
   input <- readFile "input/day8"
   let (sum, value) = process input
   putStrLn $ "Sum is " ++ show sum ++ ". Value is " ++ show value
-
-exampleString = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
